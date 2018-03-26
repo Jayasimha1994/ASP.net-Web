61f2cf48b3a08dc84c0bcd3186af4469ad66b99c
jayasimha <jayasimhajay1994@gmail.com>
2018-03-26 20:20:53 +0530

    ASP.net Project

---
+++ StudentProject1/StudentProject1/Models/StudentProject1Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StudentProject1.Models
+{
+    public class StudentProject1Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public StudentProject1Context() : base("name=StudentProject1Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<StudentProject1.Enrollment> Enrollments { get; set; }
+
+        public System.Data.Entity.DbSet<StudentProject1.Course> Courses { get; set; }
+
+        public System.Data.Entity.DbSet<StudentProject1.Student> Students { get; set; }
+    }
+}
---
+++ StudentProject1/StudentProject1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentProject1.Startup))]
+namespace StudentProject1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
